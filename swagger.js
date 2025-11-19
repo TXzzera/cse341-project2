5ea203738970dccff9b1ce6c66236ce85c93bea4
TXzzera <brunoteixeira.10@hotmail.com>
2025-11-19 17:54:11 -0300

    att

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
         title: 'Users and Cars List API',
         description: 'API for accessing users and cars informations',
     },
-    host: 'localhost:8082',
-    schemes: ['http', 'https'],
+    host: 'cse341-project2-f8lg.onrender.com/api-docs/',
+    schemes: ['https'],
     tags: [
     { name: 'Users', description: 'Manipulate informations of the users' },
     { name: 'Cars', description: 'Modify informations of the cars' },]
